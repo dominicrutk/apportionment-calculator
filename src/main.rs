6f8304e89d5c3c0639bc379339bbf38b73007432
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2022-05-21 15:09:03 -0500

    Accept command line arguments using clap

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,48 @@ use std::env;
 use std::fs;
 use std::collections::BinaryHeap;
 
+use clap::Parser;
+
+/// Apportionment calculator based on the Huntington-Hill method
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Input file
+    #[clap(short, long, default_value = "./data/us2020.tsv")]
+    input: String,
+
+    /// Output file
+    #[clap(short, long)]
+    output: Option<String>,
+
+    /// Number of seats to apportion
+    #[clap(short, long, default_value_t = 435)]
+    seats: u32,
+}
+
 fn main() {
-    // Defaults
-    let mut file_path = "./data/us2020.tsv";
-    let mut seats_remaining = 435_u32;
+    // Parse command line arguments
+    let args = Args::parse();
+    println!("Input: {}\nOutput: {}\nSeats: {}", args.input, args.output.expect("No output specified"), args.seats);
+    let mut input_file_path = args.input;
+    let mut seats_remaining = args.seats;
 
-    // User settings
-    let args: Vec<String> = env::args().collect();
-    if args.len() >= 2 {
-        file_path = &args[1];
-    }
-    if args.len() >= 3 {
-        seats_remaining = args[2].parse::<u32>().expect("You must enter a nonnegative integer number of seats.");
-    }
-
+    // Initialize priority queue based on input file
     let mut queue = BinaryHeap::new();
-    for line in fs::read_to_string(file_path).expect("Something went wrong reading the file.").lines() {
+    for line in fs::read_to_string(input_file_path).expect("Something went wrong reading the file.").lines() {
         let pair = line.split("\t").collect::<Vec<&str>>();
         let name: String = pair[0].parse().unwrap();
         let population = pair[1].parse().unwrap();
         queue.push(State::new(name, population));
     }
 
+    // Handle case where the number of seats is less than the number of states
     if queue.len() as u32 > seats_remaining {
         panic!("You must specify at least {} seats so that each state gets at least one seat.", queue.len());
     }
     seats_remaining -= queue.len() as u32;
 
+    // Apportion all remaining seats using the priority queue
     while seats_remaining > 0 {
         let mut next_state = queue.pop().unwrap();
         next_state.add_seat();
@@ -40,6 +55,7 @@ fn main() {
         seats_remaining -= 1;
     }
 
+    // Sort the states alphabetically into a list
     let mut states_list = queue.into_vec();
     states_list.sort_by(|first, second| first.get_name().cmp(second.get_name()));
 
